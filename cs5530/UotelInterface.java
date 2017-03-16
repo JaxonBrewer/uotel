f7005bc0a253d601c429354f69c2a70613cfaafe
JaxonBrewer <u0823377@utah.edu>
2017-03-15 23:55:02 -0600

    implemented login and registration

--- cs5530/UotelInterface.java
+++ cs5530/UotelInterface.java
@@ -12,30 +12,18 @@ public class UotelInterface {
 	 */
 	public static void displayLoginMenu() {
 		 System.out.println("        Welcome to the Uotel System     ");
-    	 System.out.println("1. Login:");
-    	 System.out.println("2. Register:");
-    	 System.out.println("3. Exit:");
-    	 System.out.println("pleasse enter your choice:");
+    	 System.out.println("1. Login");
+    	 System.out.println("2. Register");
+    	 System.out.println("3. Exit");
+    	 System.out.println("please enter your choice:");
 	}
 	
 	public static void displayMainMenu(String login) {
-		System.out.println("\n\n\n        Logged in as" + login);
-		System.out.println("1. Login:");
-   	 	System.out.println("2. Register:");
-   	 	System.out.println("3. Exit:");
-   	 	System.out.println("pleasse enter your choice:");		
-	}
-
-	public static String loginUser(String login, String passwd) {
-		System.out.println("Username: ");
-
-		System.out.println("Password: ");
-		return "";
-	}
-	
-	public static String registerUser(String login, String passwd, String name, String address, String phone) {
-		System.out.println("Create Username: ");
-		return "";
+		System.out.println("\n\n\n        Logged in as " + login);
+		System.out.println("1. ");
+   	 	System.out.println("2. :");
+   	 	System.out.println("3. :");
+   	 	System.out.println("please enter your choice:");		
 	}
 
 	public static void main(String[] args) {
@@ -57,18 +45,51 @@ public class UotelInterface {
             	 displayLoginMenu();
             	 while ((choice = in.readLine()) == null && choice.length() == 0);
             	 try {
-            		 c = Integer.parseInt(choice);
-            	 } catch (Exception e) {
-            		 continue;
+					 c = Integer.parseInt(choice);
+            	 } catch (Exception e) { 
+					 continue;
             	 }
-
+				 // login
 				 if (c == 1) {
-					login = loginUser();
+					System.out.print("Username: ");
+					String username = in.readLine();
+					System.out.print("Password: ");
+					String password = in.readLine();
+
+					loggedIn = Users.loginUser(username, password, con.stmt);
+					if (loggedIn) {
+						System.out.println("Login Successful");
+						login = username;
+					}
+					else {
+						System.out.println("Login Failed.");
+					}
 				 }
+				 // register
 				 else if (c == 2) {
-					login = registerUser();
+					System.out.print("Enter Username: ");
+					String username = in.readLine();
+					System.out.print("Enter Password: ");
+					String password = in.readLine();
+					System.out.print("Enter Name: ");
+					String name = in.readLine();
+					System.out.print("Enter Address: ");
+					String address = in.readLine();
+					System.out.print("Enter Phone: ");
+					String phone = in.readLine();
+					
+					loggedIn = Users.registerUser(username, password, name, address, phone, con.stmt);	
+					if (loggedIn) {
+						login = username;
+					}
+					else {
+						System.out.println("Registration Failed");
+					}
 				 }
-				 loggedIn = (login != "");
+				 else if (c == 3) {
+					 System.exit(0);
+				 }
+				 
              }
              
              while (true) {
---
+++ cs5530/Users.java
@@ -0,0 +1,54 @@
+package cs5530;
+
+import java.sql.*;
+
+public class Users {
+	public Users() {}
+
+	public static boolean loginUser(String login, String passwd, Statement stmt) {
+		String sql = "select * from Users " +
+			"where login='" + login + "' " +
+			"and password='" + passwd + "'";
+
+		boolean userExists = false;
+		ResultSet rs = null;
+
+		System.out.println("executing query: " + sql);
+		try {
+			rs = stmt.executeQuery(sql);
+			
+			userExists = rs.next();
+		}
+		catch(Exception e) {
+			System.out.println("Cannot execute login query");
+		}
+		finally {
+			try {
+				if (rs != null && !rs.isClosed())
+					rs.close();
+			}
+			catch (Exception e) {
+				System.out.println("Cannot close resultset");
+			}
+		}
+		return userExists;
+	}
+
+	public static boolean registerUser(String login, String passwd, String name, String address, String phone, Statement stmt) {
+		String sql = "insert into Users " + 
+			"values ('" + login + "','" + passwd + "','" + name + "','" + address + "','" + phone + "',false)";
+
+
+		System.out.println("executing query: " + sql);
+		try {
+			stmt.executeUpdate(sql);
+			return true;
+		}
+		catch (Exception e) {
+			System.out.println("Cannot execute register query");
+		}
+		finally {
+		}
+		return false;
+	}
+}
